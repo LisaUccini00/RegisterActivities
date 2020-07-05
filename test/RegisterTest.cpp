37ad4a7323ef88d3c0a2109422f9d4f77ca929c1
LisaUccini00 <lisa.innocenti2@stud.unifi.it>
2020-07-05 10:53:44 +0200

    Refinement test

--- test/RegisterTest.cpp
+++ test/RegisterTest.cpp
@@ -9,19 +9,26 @@
 
 
 TEST(RegisterTextFixture, TestAddActivity){
-    Time t(4, 4, 4);
-    string date = "02/12/20";
-    Activity firstEvent("title1", "fist event", t, t);
-    Activity secondEvent("title2", "second event", t, t);
+    Time start(4, 4, 4);
+    Time stop(5, 5, 5);
+    string firstDate = "06/01/20";
+    string secondDate = "02/12/20";
+    Activity firstEvent("title1", "fist event", start, stop);
+    Activity secondEvent("title2", "second event", start, stop);
     Register *reg = new Register;
     bool added;
-    added = reg->addActivity(date, &firstEvent);
-    added = reg->addActivity(date, &firstEvent);
-    added = reg->addActivity(date, &secondEvent);
+
+    added = reg->addActivity(firstDate, &firstEvent);
+    added = reg->addActivity(firstDate, &firstEvent);
+    added = reg->addActivity(firstDate, &secondEvent);
+    added = reg->addActivity(secondDate, &firstEvent);
+    added = reg->addActivity(secondDate, &secondEvent);
 
     list<Activity*> activities;
     activities.push_back(&firstEvent);
     activities.push_back(&secondEvent);
-    ASSERT_EQ(activities, reg->getActivities(date));
+
+    ASSERT_EQ(activities, reg->getActivities(firstDate));
+    ASSERT_EQ(activities, reg->getActivities(secondDate));
 }
 
